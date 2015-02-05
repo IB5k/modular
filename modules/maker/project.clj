fbf7081afe48f08f4a96fc305977ea2d7ad96054
Dylan Butman <dbutman@gmail.com>
2015-02-05 14:53:26 -0500

    cljx maker

--- modules/maker/project.clj
+++ modules/maker/project.clj
@@ -1,8 +1,16 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/maker "0.5.0"
+(defproject ib5k.modular/maker "0.5.1"
   :description "A utility library that makes it easier to blend config with kv-based component constructors."
   :url "https://github.com/juxt/modular/tree/master/modules/maker"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [])
+  :dependencies []
+  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.5.0"]]}}
+  :prep-tasks [["cljx" "once"]]
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :cljs}]})
